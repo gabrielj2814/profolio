e5a530115c1e64c8fa51c2873af143254b4dd502
Gabriel Valera <gabrielj2814@hotmail.com>
2024-07-12 19:56:10 -0400

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+// const LIVE_URL=" https://gaodevts.github.io"
+// gaodevts.github.io
 // https://astro.build/config
 export default defineConfig({
+  // site:LIVE_URL,
+  // base:"/gaodevts.github.io",
   integrations: [tailwind(), react()],
   images: {
     compression: false,
